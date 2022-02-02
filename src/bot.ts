1f5a81599d85101ed33299acf5a075806a3b91b6
Evan Williams <evanw555@gmail.com>
2022-02-01 23:16:27 -0800

    Refactor admin command processing to its own file

--- src/bot.ts
+++ src/bot.ts
@@ -3,7 +3,8 @@ import { Guild, GuildMember, Message, Snowflake, TextBasedChannels } from 'disco
 import { GoodMorningConfig, GoodMorningHistory, GoodMorningState, PlayerState, Season, TimeoutType } from './types.js';
 import TimeoutManager from './timeout-manager.js';
 import { createMidSeasonUpdateImage, createSeasonResultsImage } from './graphics.js';
-import { hasVideo, generateKMeansClusters, randInt, validateConfig, getTodayDateString, getOrderedPlayers, reactToMessage, getOrderingUpset, getOrderingUpsets, sleep, toPointsMap } from './util.js';
+import { hasVideo, randInt, validateConfig, getTodayDateString, getOrderedPlayers, reactToMessage, getOrderingUpset, sleep, toPointsMap } from './util.js';
+import processCommands from './admin.js';
 
 import { loadJson } from './load-json.js';
 const auth = loadJson('config/auth.json');
@@ -411,89 +412,10 @@ client.on('ready', async (): Promise<void> => {
     setStatus(state.isMorning);
 });
 
-const processCommands = async (msg: Message): Promise<void> => {
-    // Test out hashing of raw text input
-    if (msg.content.startsWith('#')) {
-        const exists = r9k.contains(msg.content);
-        r9k.add(msg.content);
-        messenger.reply(msg, `\`${msg.content}\` ${exists ? 'exists' : 'does *not* exist'} in the R9K text bank.`);
-        return;
-    }
-    // Test out language generation
-    if (msg.content.startsWith('$')) {
-        if (Math.random() < .5) {
-            messenger.reply(msg, languageGenerator.generate(msg.content.substring(1)));
-        } else {
-            messenger.send(msg.channel, languageGenerator.generate(msg.content.substring(1)));
-        }
-        return;
-    }
-    if (msg.content.startsWith('^')) {
-        const sanitized = msg.content.substring(1).trim();
-        const [before, after] = sanitized.split(' ');
-        messenger.reply(msg, JSON.stringify(getOrderingUpsets(before.split(','), after.split(','))));
-    }
-    // Handle sanitized commands
-    const sanitizedText: string = msg.content.trim().toLowerCase();
-    if (hasVideo(msg)) {
-        messenger.reply(msg, 'This message has video!');
-    }
-    if (sanitizedText.includes('?')) {
-        if (sanitizedText.includes('clusters')) {
-            // msg.reply(JSON.stringify(generateKMeansClusters(state.points, 3)));
-            const k: number = parseInt(sanitizedText.split(' ')[0]);
-            msg.reply(JSON.stringify(generateKMeansClusters(toPointsMap(state.players), k)));
-        }
-        else if (sanitizedText.includes('order') || sanitizedText.includes('rank') || sanitizedText.includes('winning') || sanitizedText.includes('standings')) {
-            msg.reply(getOrderedPlayers(state.players)
-                .map((key) => {
-                    return ` - <@${key}>: **${state.players[key].points}** (${state.players[key].daysSinceLastGoodMorning ?? 0}d)`;
-                })
-                .join('\n'));
-        }
-        else if (sanitizedText.includes('dump state')) {
-            await dumpState();
-        }
-        else if (sanitizedText.includes('state')) {
-            await messenger.sendLargeMonospaced(guildOwnerDmChannel, JSON.stringify(state, null, 2));
-        }
-        // Asking about points
-        else if (sanitizedText.includes('points')) {
-            const points: number = state.players[msg.author.id]?.points ?? 0;
-            if (points < 0) {
-                messenger.reply(msg, `You have **${points}** points this season... bro...`);
-            } else if (points === 0) {
-                messenger.reply(msg, `You have no points this season`);
-            } else if (points === 1) {
-                messenger.reply(msg, `You have **1** point this season`);
-            } else {
-                messenger.reply(msg, `You have **${points}** points this season`);
-            }
-        }
-        // Send mock season end messages
-        else if (sanitizedText.includes('season end')) {
-            await sendSeasonEndMessages(guildOwnerDmChannel, state);
-        }
-        // Asking about the season
-        else if (sanitizedText.includes('season')) {
-            messenger.reply(msg, `It\'s season **${state.season}**!`);
-        }
-        // Canvas stuff
-        else if (sanitizedText.includes('canvas')) {
-            await msg.channel.sendTyping();
-            const attachment = new MessageAttachment(await createMidSeasonUpdateImage(state, history.medals), 'results.png');
-            msg.reply({ files: [attachment] });
-        }
-        else if (sanitizedText.includes('react')) {
-            await reactToMessage(msg, ['🌚', '❤️', '☘️', '🌞']);
-        }
-    }
-};
-
 client.on('messageCreate', async (msg: Message): Promise<void> => {
     if (guildOwnerDmChannel && msg.channel.id === guildOwnerDmChannel.id && msg.author.id === guildOwner.id) {
         // Handle "commands" by looking for keywords
-        await processCommands(msg);
+        await processCommands(msg, state, messenger, languageGenerator, r9k);
     } else if (goodMorningChannel && msg.channel.id === goodMorningChannel.id && !msg.author.bot) {
         const userId: Snowflake = msg.author.id;
         const firstMessageThisSeason: boolean = !(userId in state.players);
