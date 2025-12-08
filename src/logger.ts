17f9debc79330f3732a80e557b53dc7fe50d3a10
Evan Williams <evanw555@gmail.com>
2025-12-08 15:22:07 -0800

    Add try-catch to logger
    Not sure what is causing the timeout issue, but this should at least cover
    a ton of areas, and it will also safeguard other utils like the messenger.

--- src/logger.ts
+++ src/logger.ts
@@ -5,9 +5,11 @@ import { TextBasedChannel } from "discord.js";
  */
 class Logger {
     private channel?: TextBasedChannel;
+    private pendingMessages: string[];
 
     setChannel(channel: TextBasedChannel): void {
         this.channel = channel;
+        this.pendingMessages = [];
     }
 
     async log(text: string): Promise<void> {
@@ -16,7 +18,21 @@ class Logger {
         }
         console.log(text);
         if (this.channel) {
-            await this.channel.send(text);
+            try {
+                await this.channel.send(text);
+                // Succeeded, so try to push the pending messages out too
+                if (this.pendingMessages.length > 0) {
+                    await this.channel.send('The following log statements failed to send previously:');
+                    for (const m of this.pendingMessages) {
+                        await this.channel.send(m);
+                    }
+                    // Clear it
+                    this.pendingMessages = [];
+                }
+            } catch (err) {
+                // Failed to send log, so add to pending list
+                this.pendingMessages.push(text);
+            }
         }
     }
 }
